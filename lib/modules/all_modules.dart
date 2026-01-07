14ed563961a6f4f772ea3f86135954af3c23d15c
Luri Darmawan <luri@carik.id>
2026-01-08 05:22:32 +0700

    update icon launcher

--- lib/modules/all_modules.dart
+++ lib/modules/all_modules.dart
@@ -1,14 +1,14 @@
 import 'module_registry.dart';
 import 'news/news_module.dart';
-import 'sample/demo_module.dart';
-import 'sample/sample_module.dart';
+// import 'sample/demo_module.dart';
+// import 'sample/sample_module.dart';
+import 'package:arrow_sense/arrow_sense_module.dart';
 
 /// Auto-generated file. Do not edit manually.
 /// This file registers all available modules to the registry.
 class ModuleManifest {
   static void register() {
-    ModuleRegistry.register(DemoModule());
+    ModuleRegistry.register(ArrowSenseModule());
     ModuleRegistry.register(NewsModule());
-    ModuleRegistry.register(SampleModule());
   }
 }
